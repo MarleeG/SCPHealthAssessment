dcb2dc882062578a7b420f2008b2a7feb7badb7f
Marlee Gerard <marleegerard@gmail.com>
2021-07-01 19:33:27 -0400

    initial commit

---
+++ client/src/components/Input.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Input = () => {
+  // eslint-disable-next-line
+  const [numbers, setNumbers] = useState([]);
+
+  return <div> I am the Input component</div>;
+};
+
+export default Input;
