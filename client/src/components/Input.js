5d295ae93bf2ae35fa561b339441e9cd755b91ab
Marlee Gerard <marleegerard@gmail.com>
2021-07-01 21:41:49 -0400

    checked if input is a number in dev tools

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,19 +1,41 @@
 import React, { useState } from "react";
 
 import "./Input.css";
+const { log } = console;
 const Input = () => {
-  // eslint-disable-next-line
   const [numbers, setNumbers] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [isInputANumber, setIsInputANumber] = useState(false);
+  const [inputValue, setInputValue] = useState(0);
+
+  const checkIfInputIsANumber = (value) => {
+    const updatedValue = parseFloat(value);
+
+    if (!isNaN(updatedValue)) {
+      log(updatedValue);
+      log("is a number");
+
+      setIsInputANumber(true);
+    } else {
+      log(updatedValue);
+      log("is not a number");
+
+      setIsInputANumber(false);
+    }
+  };
 
   const handleChange = (e) => {
     const { value } = e.target;
     setInputValue(value);
+
+    checkIfInputIsANumber(value);
   };
 
   return (
     <div>
       {" "}
+      <strong>
+        <label style={{ display: "block" }}>Enter a valid number</label>
+      </strong>
       <input
         onChange={handleChange}
         value={inputValue}
