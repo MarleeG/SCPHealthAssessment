b12789ca78599f9edaff126f73afbb8d0245fdcd
Marlee Gerard <marleegerard@gmail.com>
2021-07-05 21:49:43 -0400

    UI is now formatted and we are now displayig lafayette data

--- client/src/API.js
+++ client/src/API.js
@@ -6,7 +6,7 @@ const log = console;
 const data = {
   getAllLafayetteData: () => {
     const allData = axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?lat=30.2240897&lon=-92.01984270000003&cnt=24&appid=${process.env.REACT_APP_APP_ID}`
+      `http://api.openweathermap.org/data/2.5/forecast?lat=30.2240897&lon=-92.01984270000003&cnt=25&units=imperial&appid=${process.env.REACT_APP_APP_ID}`
     );
 
     return allData;
--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect, Fragment } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import API from "../API";
 import { mean, median, mode } from "./Index.js";
 
@@ -9,6 +9,14 @@ const Input = () => {
   const [numbers, setNumbers] = useState([]);
   const [isInputANumber, setIsInputANumber] = useState(false);
   const [inputValue, setInputValue] = useState(0);
+  const [showCalculatedDataCol, setShowCalculatedDataCol] = useState(false);
+  const [stats, setStats] = useState({
+    mean: null,
+    median: null,
+    mode: null,
+    beginningDate: null,
+    endingDate: null,
+  });
 
   const checkIfInputIsANumber = (value) => {
     const updatedValue = parseFloat(value);
@@ -41,6 +49,18 @@ const Input = () => {
     }
   };
 
+  const dataComponent = (title, beginningDate, endingDate) => {
+    const component = (
+      <Fragment>
+        <h4>{title}</h4>
+        <strong>Dates: {`${beginningDate} - ${endingDate}`}</strong>
+        <p>Mean: {stats.mean}</p> <p>Median: {stats.median}</p>{" "}
+        <p style={{overflowWrap: "break-word"}}>Mode: {stats.mode.join(", ")}{" "}</p>
+      </Fragment>
+    );
+    return component;
+  };
+
   const getMedianMeanMode = () => {
     log(`MEAN: ${mean(numbers)}`);
     log("MODE:", mode(numbers));
@@ -49,6 +69,7 @@ const Input = () => {
   };
 
   const getAllLafayetteData = async () => {
+    setShowCalculatedDataCol(true);
     let res;
     try {
       res = await API.getAllLafayetteData();
@@ -56,51 +77,101 @@ const Input = () => {
       log(err);
     }
 
-    console.log(res);
+    const data = res.data.list;
+    const beginningDate = res.data.list[0].dt_txt;
+    const endingDate = res.data.list[res.data.list.length - 1].dt_txt;
+
+    let temperatures = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const curretTemp = data[i].main.temp;
+      temperatures.push(curretTemp);
+    }
+
+    setStats({
+      ...stats,
+      beginningDate: beginningDate,
+      endingDate: endingDate,
+      mean: mean(temperatures),
+      median: median(temperatures),
+      mode: mode(temperatures),
+    });
+
+    // console.log(res.data.list);
+    log(temperatures);
   };
 
   useEffect(() => {
-    getAllLafayetteData()
+    // getAllLafayetteData();
   }, [numbers, inputValue]);
-
-  // const Form = () => {
-  //   return <Form>hello</Form>;
-  // };
 
   return (
     <div>
-      <strong>
-        <label style={{ display: "block" }}>Enter a valid number</label>
-      </strong>
-      <input
-        onChange={handleChange}
-        value={inputValue}
-        placeholder="Enter a number"
-        name="numbers_input"
-      />
-      <Button
-        className="mx-2"
-        variant="dark"
-        type="submit"
-        disabled={!isInputANumber}
-        onClick={(e) => handleSubmit(e)}
-      >
-        Submit
-      </Button>
+      <Container>
+        <Row>
+          <Col lg={12} className="my-4">
+            <strong>
+              <label className="mx-2">Enter a valid zip code</label>
+            </strong>
+            <input
+              onChange={handleChange}
+              value={inputValue}
+              placeholder="Enter a zip code"
+              name="numbers_input"
+            />
 
-      <Button
-        variant="primary"
-        size="md"
-        onClick={() => {
-          getMedianMeanMode();
-        }}
-        disabled={!(numbers.length > 0)}
-      >
-        {" "}
-        Get Median, Mean, and Mode
-      </Button>
+            <Button
+              className="mx-2"
+              variant="dark"
+              type="submit"
+              disabled={!isInputANumber}
+              onClick={(e) => handleSubmit(e)}
+            >
+              Submit
+            </Button>
+          </Col>
+        </Row>
 
-      {numbers.length > 0 && (
+        <Row>
+          <Col lg={showCalculatedDataCol ? 6 : 12}>
+            {/* <Button
+              variant="primary"
+              size="md"
+              onClick={() => {
+                getMedianMeanMode();
+              }}
+              disabled={!(numbers.length > 0)}
+            >
+              {" "}
+              Get Median, Mean, and Mode
+            </Button> */}
+
+            <Button
+              variant="dark"
+              size="lg"
+              onClick={() => {
+                getAllLafayetteData();
+              }}
+              // disabled={!(numbers.length > 0)}
+            >
+              {" "}
+              Get Median, Mean, and Mode for Lafayette
+            </Button>
+          </Col>
+
+          {showCalculatedDataCol && (
+            <Col lg={6}>
+              {dataComponent(
+                "Lafayette 3 day forcast",
+                stats.beginningDate,
+                stats.endingDate
+              )}
+            </Col>
+          )}
+        </Row>
+      </Container>
+
+      {/* {numbers.length > 0 && (
         <div className="numbers-list my-5">
           {numbers.map((num, idx) => {
             return (
@@ -110,7 +181,7 @@ const Input = () => {
             );
           })}
         </div>
-      )}
+      )} */}
     </div>
   );
 };
