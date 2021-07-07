fd2c3d596d69a98d9050ace70b7bac8d4bec62b7
Marlee Gerard <marleegerard@gmail.com>
2021-07-06 20:32:37 -0400

    zip based data is now displaying in the UI

--- client/src/API.js
+++ client/src/API.js
@@ -5,11 +5,15 @@ dotenv.config();
 const log = console;
 const data = {
   getAllLafayetteData: () => {
-    const allData = axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?lat=30.2240897&lon=-92.01984270000003&cnt=25&units=imperial&appid=${process.env.REACT_APP_APP_ID}`
-    );
+    const URL = `http://api.openweathermap.org/data/2.5/forecast?lat=30.2240897&lon=-92.01984270000003&cnt=25&units=imperial&appid=${process.env.REACT_APP_APP_ID}`;
+    const allData = axios.get(URL);
 
     return allData;
+  },
+  getDataByZipCode: (zip) => {
+    const URL = `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&cnt=25&units=imperial&appid=${process.env.REACT_APP_APP_ID}`;
+
+    return axios.get(URL);
   },
 };
 
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
+const apiRoutes = require("./routes/api");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(apiRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
