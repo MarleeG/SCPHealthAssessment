99cf3a9bbbc0e67d33d7329898dd5ed6abffff26
Marlee Gerard <marleegerard@gmail.com>
2021-07-07 20:10:44 -0400

    fixing api for prod

--- client/src/API.js
+++ client/src/API.js
@@ -11,13 +11,16 @@ const PORT = process.env.NODE_ENV === 'production'? process.env.PORT : 5000;
 const data = {
   getAllLafayetteData: () => {
     const FULL_URL = `${BASE_URL}:${PORT}/api`;
+    console.log(`LF: ${FULL_URL}`);
     const allData = axios.get(FULL_URL)
     return allData;
   },
 
   getDataByZipCode: (zip) => {
     // const dataByZip = axios.get(`http://localhost:5000/api/${zip}`);
-    const FULL_URL = `${BASE_URL}:${PORT}/api/${zip}`
+    const FULL_URL = `${BASE_URL}:${PORT}/api/${zip}`;
+    console.log(`ZIP: ${FULL_URL}`);
+
     const dataByZip = axios.get(FULL_URL);
 
 
