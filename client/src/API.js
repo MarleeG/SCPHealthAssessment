daa45404dc2760a3c6beb5ebad5dc432740cf695
Marlee Gerard <marleegerard@gmail.com>
2021-07-07 20:12:24 -0400

    fixing api for prod

--- client/src/API.js
+++ client/src/API.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 // import dotenv from "dotenv";
-// dotenv.config();
 
 const production = "https://scphealthassessment-mg.herokuapp.com";
 const development = "http://localhost";
